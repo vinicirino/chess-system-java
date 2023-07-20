4b029adb57c8937d9423ed53988d6ead5c2ae527
vinicirino <vinicirino@gmail.com>
2023-07-20 19:12:30 -0300

    classe position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("first commit");
+
+		Position p = new Position(2,3);
+		
+		System.out.println(p);
+		
 	}
 
 }
