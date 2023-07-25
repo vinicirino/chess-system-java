e5461f8e5cd93800e96f4636d40fca7a75e65bb5
vinicirino <vinicirino@gmail.com>
2023-07-25 20:43:22 -0300

    moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,32 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	ChessMatch chessMatch = new ChessMatch();
-	UI.printBoard(chessMatch.getPieces());
-	
+		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+
+		while (true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			
+		}
+
 	}
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -47,11 +47,23 @@ public class Board {
 			throw new BoardException("there is already a piece on position "+ position);
 		}
 		
-		
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
-			
 	}
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(position)==null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 	
 	private boolean positionExistis(int row, int column) {
 		return row >=0 && row< rows && column >=0 && column < columns;
